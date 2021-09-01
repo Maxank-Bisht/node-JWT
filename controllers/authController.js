9e6a03d25556abddfc5d1284aeda34a8d55b79d7
Mayank Bisht <bishtmayank799@gmail.com>
2021-09-01 17:19:44 +0530

    created jwt for signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,8 @@
+require('dotenv').config();
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+const jwt_secret = process.env.JWT_SECRET;
 
 //error handler
 const handleErrors = (err) => {
@@ -19,6 +23,14 @@ const handleErrors = (err) => {
 	return errors;
 };
 
+const maxAge = 3 * 24 * 60 * 60;
+//creating web tokens
+const createToken = (id) => {
+	return jwt.sign({ id }, jwt_secret, {
+		expiresIn: maxAge,
+	});
+};
+
 module.exports.signup_get = (req, res) => {
 	res.render('signup');
 };
@@ -30,7 +42,9 @@ module.exports.signup_post = async (req, res) => {
 
 	try {
 		const user = await User.create({ email, password });
-		res.status(201).json(user);
+		const token = createToken(user._id);
+		res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+		res.status(201).json({ user: user._id });
 	} catch (error) {
 		const errors = handleErrors(error);
 		res.status(400).json({ errors });
