ec6c31892d1869b338def888c72fe16647153940
Mayank Bisht <bishtmayank799@gmail.com>
2021-09-02 21:01:27 +0530

    protected routes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
 
 const db_username = process.env.DATABASE_USERNAME;
 const db_password = process.env.DATABASE_PASSWORD;
@@ -29,6 +30,7 @@ mongoose
 	.catch((err) => console.log(err));
 
 // routes
+app.get('*', checkUser);
 app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
 app.use(authRoutes);
